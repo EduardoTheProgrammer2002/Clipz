d9fe28009a83938ceb6b45220f286622543903cc
Eduardo Rodriguez <edwardo16blak@gmail.com>
2022-10-28 14:14:29 -0700

    End - Section 7: Master Project: Reactive Forms.

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,12 +7,52 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent  {
+  // Alert Props
+  showAlert = false
+  alertMsg = 'Please wait! Your account is being created'
+  alertColor = 'blue'
+
+  //Forms controls for the inputs
+  name = new FormControl('', [
+    Validators.minLength(3),
+    Validators.required
+  ])
+  email = new FormControl('', [
+    Validators.required,
+    Validators.email
+  ])
+  age = new FormControl('', [
+    Validators.required,
+    Validators.min(16),
+    Validators.max(100)
+  ])
+  phoneNumber = new FormControl('', [
+    Validators.required,
+    Validators.minLength(13),
+    Validators.maxLength(13)
+  ])
+  password = new FormControl('', [
+    Validators.required,
+    Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm)
+  ])
+  confirmPassword = new FormControl('', [
+    Validators.required
+  ])
+
+  //Form group to register the Form
   registerForm = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    Age: new FormControl(''),
-    phoneNumber: new FormControl(''),
-    password: new FormControl(''),
-    confirmPassword: new FormControl('')
+    name: this.name,
+    email: this.email,
+    Age: this.age,
+    phoneNumber: this.phoneNumber,
+    password: this.password,
+    confirmPassword: this.confirmPassword
   })
+
+  register() {
+    this.showAlert = true;
+    this.alertMsg = 'Please wait! Your account is being created.'
+    this.alertColor = 'blue'
+    
+  }
 }
