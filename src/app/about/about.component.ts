22cb55fe8dbf55a99833a92abe0aee0109f00950
EduardoTheProgrammer2002 <edwardo16blak@gmail.com>
2022-12-09 11:23:16 -0800

    This stylied the about page.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+  socialMedia = [
+    {icon: 'fa-brands fa-facebook', link: 'https://www.facebook.com/edwardo.rodriguez.9480'},
+    {icon:'fa-brands fa-instagram', link: 'https://www.instagram.com/eduardo.dr.k/'},
+    {icon:'fa-brands fa-github', link: 'https://github.com/EduardoTheProgrammer2002'}, 
+    {icon:'fa-brands fa-linkedin', link: 'https://www.linkedin.com/in/eduardo-rodr%C3%ADguez-583382234/'}
+  ]
 
   constructor() { }
 
